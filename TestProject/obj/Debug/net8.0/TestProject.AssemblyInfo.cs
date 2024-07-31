eb1cb3e3dfce70bc5f47a9cc0dc17b130903bf73
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-07-31 20:05:46 -0300

    .

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c34f2099675fa278875ab998086bf6994fd7746")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe4cae696fb79417a3b2a146253743cc1e66e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
